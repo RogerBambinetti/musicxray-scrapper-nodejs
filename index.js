2fd571ac6e42b1bdfe0c096983b2b732291d5a18
Roger <rogerbambinetti@hotmail.com>
2024-07-29 15:50:11 -0300

    add sitemaps

--- index.js
+++ index.js
@@ -8,38 +8,42 @@ const agent = new https.Agent({
     rejectUnauthorized: false
 });
 
-let currentID = 1247011;
-const tracks = [];
+let tracks = [];
+
+const sitemap = require('./sitemaps/sitemap1.json');
+
+async function request(url) {
+    const response = await axios.get(url, { httpsAgent: agent });
+
+    if (response.status === 200) {
+        try {
+
+            const doc = new jsdom.JSDOM(response.data)
+            const metadata = doc.window.document.querySelectorAll('.metadata');
+
+            const trackInfo = {
+                url: url,
+                artist: metadata.item(1).innerHTML,
+                album: metadata.item(2).innerHTML,
+                title: metadata.item(3).innerHTML,
+                extra: doc.window.document.querySelector('.box.grey-outline').innerHTML
+            }
+
+            return trackInfo;
+        } catch (e) {
+            console.log(e);
+        }
+    }
+}
 
 async function init() {
-    do {
-        const response = await axios.get(`https://www.musicxray.com/xrays/${currentID}`, { httpsAgent: agent });
+    for (const url of sitemap) {
 
-        if (response.status === 200) {
-            try {
+        const result = await request(url);
+        tracks.push(result);
 
-                const doc = new jsdom.JSDOM(response.data)
-                const metadata = doc.window.document.querySelectorAll('.metadata');
-
-                const trackInfo = {
-                    ID: currentID,
-                    artist: metadata.item(1).innerHTML,
-                    album: metadata.item(2).innerHTML,
-                    title: metadata.item(3).innerHTML,
-                    //extra: doc.window.document.querySelector('.box.grey-outline').innerHTML
-                }
-
-                tracks.push(trackInfo);
-
-                await fs.writeFileSync('./logs/logs.json', JSON.stringify(tracks, null, 4), 'utf8');
-
-            } catch (e) {
-
-            }
-        }
-
-        currentID++;
-    } while (true);
+        await fs.writeFileSync(`./logs/logs-sitemap1.json`, JSON.stringify(tracks, null, 4), 'utf8');
+    }
 }
 
 init();
